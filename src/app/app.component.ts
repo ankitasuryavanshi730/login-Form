842d5296309abcaff3e4a5aca78170fad32a9688
Ankita Suryavanshi <ankitasuryavanshi730@gmail.com>
2023-03-27 11:25:49 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'login_Task';
+export class AppComponent  implements OnInit {
+   logform: any;
+    loginArray:any=[]
+
+
+
+
+
+
+
+
+
+
+
+
+   ngOnInit(): void {
+
+
+    this.logform = new FormGroup({
+
+      username: new FormControl("" , Validators.compose([Validators.required ,Validators.email])),
+      password: new FormControl("", Validators.compose([Validators.required]))
+
+
+    })
+   }
+
+
+
+
+
+  submit(data: any){
+
+    // console.log(data);
+
+    this.loginArray.push(data)
+    console.log(this.loginArray);
+
+    localStorage.setItem("login" , JSON.stringify(data));
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
 }
---
+++ src/app/storedata/storedata.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-storedata',
+  templateUrl: './storedata.component.html',
+  styleUrls: ['./storedata.component.css']
+})
+export class StoredataComponent implements OnInit {
+
+
+@Input() loginArray:any = [''];
+
+
+
+
+
+
+ngOnInit(): void {
+}
+
+}
